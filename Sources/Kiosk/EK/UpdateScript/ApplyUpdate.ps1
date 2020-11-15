956e065f2afdc0d3255fa5fb15fd1523e1af1651
IGOR_NOYT\Igor <vynokurov83@gmail.com>
2020-11-15 18:22:39 +0200

    update script

---
+++ Sources/Kiosk/EK/UpdateScript/ApplyUpdate.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = "KioskApp"
+$targetLocation = Get-Location
+
+$DependencyPackages = Get-ChildItem (Join-Path (Join-Path $targetLocation "Dependencies") "*.appx")
+
+if ($DependencyPackages.Count -gt 0)
+{
+    Add-AppxPackage -Path "$targetLocation\$packageName.appx" -DependencyPath $DependencyPackages.FullName -ForceApplicationShutdown -Verbose
+}
+else
+{
+    Add-AppxPackage -Path "$targetLocation\$packageName.appx" -ForceApplicationShutdown -Verbose
+}
+
+Restart-Computer -Force
